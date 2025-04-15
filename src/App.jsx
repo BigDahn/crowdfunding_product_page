799a28b7d83ccf15e6510cc17dc620c9f65e2c41
BigDahn <harkinborholarholuwah@gmail.com>
2025-04-15 22:45:46 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,93 @@ function App() {
             <p className="text-[10px]">days left</p>
           </div>
         </div>
-        <div class="bg-red-300 h-[30vh] rounded-md ">dfdf</div>
+        <div class="bg-[#ffffff] rounded-md  shadow-lg px-10 py-8 flex flex-col gap-6">
+          <h3 className="text-[16px] font-bold font-Commissioner">
+            About this project
+          </h3>
+          <p className="text-[10px] text-[#9b9b9b] font-medium font-Commissioner">
+            The Mastercraft Bamboo Monitor Riser is a sturdy and stylish
+            platform that elevates your screen to a more comfortable viewing
+            height. Placing your monitor at eye level has the potential to
+            improve your posture and make you more comfortable while at work,
+            helping you stay focused on the task at hand.
+          </p>
+          <p className="text-[10px] text-[#9b9b9b] font-medium font-Commissioner">
+            Featuring artisan craftsmanship, the simplicity of design creates
+            extra desk space below your computer to allow notepads, pens, and
+            USB sticks to be stored under the stand.
+          </p>
+          <article className="border border-gray-200 rounded-md flex flex-col px-6 py-4 gap-4">
+            <div className="flex justify-between items-center">
+              <h3 className="font-bold text-[13px] font-Commissioner">
+                Bamboo Stand
+              </h3>
+              <h6 className="text-[11px] text-[#73b9b0] font-medium">
+                Pledge $25 or more
+              </h6>
+            </div>
+
+            <p className="text-[10px] text-[#9b9b9b] font-medium font-Commissioner leading-5">
+              You get an ergonomic stand made of natural bamboo. You've helped
+              us launch our promotional campaign and you'll be added to a
+              special Backer member list
+            </p>
+            <div className="flex justify-between items-center">
+              <h2 className="font-bold text-[16px] flex gap-1 items-start">
+                101 <span className="text-[9px] text-[#9b9b9b] mt-1">left</span>
+              </h2>
+              <button className="rounded-full bg-[#157a74] text-white text-[9px] font-Commissioner font-bold px-[15px] py-[0.7em]">
+                Select Reward
+              </button>
+            </div>
+          </article>
+          <article className="border border-gray-200 rounded-md flex flex-col px-5 py-4 gap-4 ">
+            <div className="flex justify-between items-center">
+              <h3 className="font-bold text-[13px] font-Commissioner">
+                Black Edition Stand
+              </h3>
+              <h6 className="text-[11px] text-[#73b9b0] font-medium">
+                Pledge $75 or more
+              </h6>
+            </div>
+            <p className="text-[10px] text-[#9b9b9b] font-medium font-Commissioner leading-5">
+              You get a Black Special Edition computer stand and a personal
+              thank you. You'll be added to our Backer member list. Shipping is
+              included
+            </p>
+            <div className="flex justify-between items-center">
+              <h5 className="font-bold text-[16px] flex gap-1 items-start">
+                64 <span className="text-[9px] text-[#9b9b9b] mt-1">left</span>
+              </h5>
+              <button className="rounded-full bg-[#157a74] text-white text-[9px] font-Commissioner font-bold px-[15px] py-[0.7em]">
+                Select Reward
+              </button>
+            </div>
+          </article>
+          <article className="border border-gray-200 rounded-md flex flex-col px-6 py-4 gap-4">
+            <div className="flex justify-between items-center">
+              <h3 className="font-bold text-[13px] font-Commissioner">
+                Mahogany Special Edition
+              </h3>
+              <h5 className="text-[11px] text-[#73b9b0] font-medium">
+                Pledge $200 or more
+              </h5>
+            </div>
+            <p className="text-[10px] text-[#9b9b9b] font-medium font-Commissioner leading-5">
+              You get two Special Edition Mahogany stands, a Backer T-Shirt and
+              a personal thank you. You'll be added to our Backer member list.
+              Shipping is included
+            </p>
+            <div className="flex justify-between items-center">
+              <h5 className="font-bold text-[16px] text-[#9b9b9b] flex gap-1 items-start">
+                0 <span className="text-[9px] text-[#9b9b9b] mt-1">left</span>
+              </h5>
+              <button className="rounded-full bg-[#9b9b9b] text-white text-[9px] font-Commissioner font-bold px-[15px] py-[0.7em]">
+                Out of Stock
+              </button>
+            </div>
+          </article>
+        </div>
       </section>
     </div>
   );
